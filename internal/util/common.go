eba39ffe1fce07abd8055d95a393be1e3f214775
Hardy Ferentschik <hardy@hibernate.org>
2018-02-20 15:25:26 +0100

    Issue #168 Changing private key id and fixing osioUrl

--- internal/util/common.go
+++ internal/util/common.go
@@ -19,7 +19,7 @@ var (
 
 func init() {
 	environments[stage] = environment{
-		osioUrl:      "https://openshift.io",
+		osioUrl:      "https://prod-preview.openshift.io",
 		apiUrl:       "https://api.openshift.io",
 		authURL:      "https://auth.prod-preview.openshift.io",
 		redirectURL:  "https://jenkins.prod-preview.openshift.io",
@@ -27,10 +27,10 @@ func init() {
 	}
 
 	environments[prod] = environment{
-		osioUrl:      "https://prod-preview.openshift.io",
+		osioUrl:      "https://openshift.io",
 		apiUrl:       "https://api.prod-preview.openshift.io",
 		authURL:      "https://auth.openshift.io",
 		redirectURL:  "https://jenkins.openshift.io",
-		privateKeyId: "0lL0vXs9YRVqZMowyw8uNLR_yr0iFaozdQk9rzq2OVU",
+		privateKeyId: "quzUZlR_ollAUoAGgm165tYDTU3xtKon8O1RghJZ4TU",
 	}
 }
