0428d757981dbac1161ebec37d97de4765e96cba
shoubhik bose <sbose78@gmail.com>
2018-05-11 23:08:42 +0530

    Issue #257 add tests for server

--- internal/router/router.go
+++ internal/router/router.go
@@ -1,7 +1,10 @@
 package router
 
 import (
+	"net/http"
+
 	"github.com/fabric8-services/fabric8-jenkins-proxy/internal/api"
+	"github.com/fabric8-services/fabric8-jenkins-proxy/internal/proxy"
 	"github.com/julienschmidt/httprouter"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
@@ -13,4 +16,12 @@ func CreateAPIRouter(api api.ProxyAPI) *httprouter.Router {
 	proxyRouter.GET("/api/info/:namespace", api.Info)
 	proxyRouter.Handler("GET", "/metrics", promhttp.Handler())
 	return proxyRouter
+}
+
+// CreateProxyRouter is the HTTP server handler which handles the incoming webhook and UI requests.
+func CreateProxyRouter(proxy *proxy.Proxy) *http.ServeMux {
+	proxyMux := http.NewServeMux()
+	proxyMux.HandleFunc("/", proxy.Handle)
+
+	return proxyMux
 }
